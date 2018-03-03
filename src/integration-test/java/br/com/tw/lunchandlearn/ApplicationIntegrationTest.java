18e60fabeb87863518e123990639e5078ccaae5f
Edlaine Zamora <ezamora@thoughtworks.com>
2018-03-03 02:51:34 -0300

    ISSUE-20 @edlainezamora added prod profile for to test mongo db

--- src/integration-test/java/br/com/tw/lunchandlearn/ApplicationIntegrationTest.java
+++ src/integration-test/java/br/com/tw/lunchandlearn/ApplicationIntegrationTest.java
@@ -4,9 +4,7 @@ import br.com.tw.lunchandlearn.endpoint.Home;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
